fdd582257e7ef72047a598725c234c510f6931a9
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2020-05-03 20:23:27 -0500

    Iteration 0 complete. The ToDo list appears as it should, but is still static.

---
+++ ToDoListApp_VanHook/ToDo2.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo2.swift
+//  ToDoListApp_VanHook
+//
+//  Created by Kaitlyn VanHook on 5/3/20.
+//  Copyright © 2020 Kaitlyn VanHook. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
